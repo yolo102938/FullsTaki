d045933856e3e9fe739fd65d4da041ad147721b4
unknown <jonathan.ginzburg@gmail.com>
2023-04-30 06:16:29 +0300

    Finished all .h, next step implementation.

--- FullsTaki/FullsTaki/Communicator.h
+++ FullsTaki/FullsTaki/Communicator.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <map>
+#include <windows.h>
+#include "RequestHandlerFactory.h"
+class Communicator
+{
+public:
+	Communicator();
+	~Communicator();
+	void startHandleRequests();
+	void bindAndListen();
+	void handleNewClient(SOCKET);
+private:
+	SOCKET m_serverSocket;
+	map<SOCKET, IRequestHandler*> m_clients;
+	SOCKET m_serverSocket;
+};
