aeb25b16477df099d886266056cd39c3223041cc
Ohad Shaked <ohad.saked@gmail.com>
2023-06-13 16:31:28 +0300

    updated all responses for the game

--- FullsTaki/FullsTaki/ResponseTypes.hpp
+++ FullsTaki/FullsTaki/ResponseTypes.hpp
@@ -9,6 +9,7 @@ using std::endl;
 using std::string;
 
 
+
 typedef struct ErrorResponse
 {
 	string message;
@@ -39,49 +40,109 @@ typedef struct SignupResponse
 	}
 } SignupResponse;
 
-typedef struct LogoutResponse {
+typedef struct GetPersonalStatsResponse
+{
+	unsigned int status;
+	vector<string> statistics;
+
+	void print() const
+	{
+		cout << "Response Type: GetPersonalStatsResponse\nStatus: " + status << endl;
+	}
+} GetPersonalStatsResponse;
+
+typedef struct LeaveGameResponse
+{
+	unsigned int status;
+
+	void print() const
+	{
+		cout << "Response Type: LeaveGameResponse\nStatus: " + status << endl;
+	}
+} LeaveGameResponse;
+
+struct PlayerResults
+{
+	string username;
+	unsigned int cardsInHand;
+	float averagePlayTime;
+};
+
+struct DrawCardFromDeckResponse
+{
+	unsigned int status;
+	string card;
+};
+
+struct PlaceCardResponse
+{
+	unsigned int status;
+};
+
+struct GetGameResultsResponse
+{
+	unsigned int status;
+	vector<PlayerResults> results;
+};
+
+struct CallLastCardInHandResponse
+{
+	unsigned int status;
+};
+
+typedef struct LogoutResponse
+{
 	unsigned int status;
 } LogoutResponse;
 
-typedef struct JoinRoomResponse {
+typedef struct JoinRoomResponse
+{
 	unsigned int status;
 } JoinRoomResponse;
 
-typedef struct GetPlayersInRoomResponse {
-	std::vector<std::string> players;
+typedef struct GetPlayersInRoomResponse
+{
+	vector<string> players;
 } GetPlayersInRoomResponse;
 
-typedef struct CreateRoomResponse {
+typedef struct CreateRoomResponse
+{
 	unsigned int status;
 } CreateRoomResponse;
 
-typedef struct GetRoomsResponse {
+typedef struct GetRoomsResponse
+{
 	unsigned int status;
-	std::vector<RoomData> rooms;
+	vector<RoomData> rooms;
 } GetRoomsResponse;
 
-typedef struct getHighScoreResponse {
+typedef struct getHighScoreResponse
+{
 	unsigned int status;
-	std::vector<std::string> statistics;
+	vector<string> statistics;
 } getHighScoreResponse;
 
 
-typedef struct StartGameResponse {
+typedef struct StartGameResponse
+{
     unsigned int status;
 } StartGameResponse;
 
-typedef struct CloseRoomResponse {
+typedef struct CloseRoomResponse
+{
     unsigned int status;
 } CloseRoomResponse;
 
-typedef struct GetRoomStateResponse {
+typedef struct GetRoomStateResponse
+{
     unsigned int status;
     bool hasGameBegun;
-    std::vector<std::string> players;
+    vector<string> players;
 
 } GetRoomStateResponse;
 
-typedef struct LeaveRoomResponse {
+typedef struct LeaveRoomResponse
+{
     unsigned int status;
 
 } LeaveRoomResponse;
